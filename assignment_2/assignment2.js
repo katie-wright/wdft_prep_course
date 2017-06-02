41e80a8987c2e2e9eb51a3ef968769940c56256d
katie-wright <ka.wright@hotmail.com>
2017-06-02 11:59:05 -0400

    complete assignment2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,7 +2,9 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
+for (i=1; i<=100; i++) {
+    console.log(i);
+}
 
 
 /*
@@ -17,3 +19,13 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+for (i=1; i<=7; i++) {
+    if (i==1) {
+        var k = '#';
+    }
+    else {
+        var j = '#';
+        var k = k + j;   
+    }
+    console.log(k);
+}
